1dcf154af6ad16a011cfa1e0f88d368d0f0af66c
Gayeong98 <gayeong122@gmail.com>
2023-05-08 17:20:46 +0900

    fix: AnalysisBusinessView

--- src/views/AnalysisView/AnalysisBusinessView/index.tsx
+++ src/views/AnalysisView/AnalysisBusinessView/index.tsx
@@ -1,10 +1,25 @@
 import { Box } from '@mui/material'
+import { LocalizationProvider, StaticDateTimePicker } from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import React from 'react'
+import SaleAnalysisDetail from '../SaleAnalysisView/SaleAnalysisDetail';
+import AnalysisBusinessDetail from './AnalysisBusinessDetail';
 
 export default function AnalysisBusinessView() {
-  return (
-    <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyItems: 'center', alignItems: 'center'}}>
-        
-    </Box>
-  )
+    return (
+        <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyItems: 'center', alignItems: 'center' }}>
+            <Box sx={{ width: '500px' }}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DemoContainer components={['StaticDateTimePicker']}>
+                        <DemoItem>
+                            <StaticDateTimePicker defaultValue={dayjs('2022-04-17T15:30')} />
+                        </DemoItem>
+                    </DemoContainer>
+                </LocalizationProvider>
+            </Box>
+            <AnalysisBusinessDetail />
+        </Box>
+    )
 }
--- src/views/AnalysisView/index.tsx
+++ src/views/AnalysisView/index.tsx
@@ -2,17 +2,19 @@ import { Box, Divider } from '@mui/material'
 import React from 'react'
 import AnalysisCategoryBar from './AnalysisCategoryBar'
 import SaleAnalysisView from './SaleAnalysisView'
+import AnalysisBusinessView from './AnalysisBusinessView'
 
 export default function AnalysisView() {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', height: '88vh' }}>
+    <Box sx={{display: 'flex', flexDirection: 'column', height: '88vh', overflow: 'auto' }}>
       <Box sx={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
         <Box sx={{width: '35vh'}}>
           <AnalysisCategoryBar />
         </Box>
         <Divider orientation='vertical' flexItem />
         <Box sx={{ flexGrow: 1}}>
-          <SaleAnalysisView />
+          {/* <SaleAnalysisView /> */}
+          <AnalysisBusinessView />
         </Box>
       </Box>
     </Box>
