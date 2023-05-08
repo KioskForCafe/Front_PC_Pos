e8b0a2e0d7ae06115d9b7f41e98a1d034b1978d3
Gayeong98 <gayeong122@gmail.com>
2023-05-08 17:51:42 +0900

    fix: SaleAnalysisView, AnalysisBusinessView

--- src/views/AnalysisView/AnalysisProductView/index.tsx
+++ src/views/AnalysisView/AnalysisProductView/index.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material'
-import { LocalizationProvider, StaticDateTimePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers';
 import dayjs from 'dayjs';
 import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -11,9 +11,9 @@ export default function AnalysisProductView() {
         <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyItems: 'center', alignItems: 'center' }}>
             <Box sx={{ width: '500px' }}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DemoContainer components={['StaticDateTimePicker']}>
+                    <DemoContainer components={['StaticDatePicker']}>
                         <DemoItem>
-                            <StaticDateTimePicker defaultValue={dayjs('2022-04-17T15:30')} />
+                            <StaticDatePicker defaultValue={dayjs('2022-04-17')} />
                         </DemoItem>
                     </DemoContainer>
                 </LocalizationProvider>
--- src/views/AnalysisView/SaleAnalysisView/index.tsx
+++ src/views/AnalysisView/SaleAnalysisView/index.tsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material'
 import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import React from 'react'
-import { LocalizationProvider, StaticDateTimePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers';
 import dayjs from 'dayjs';
 import SaleAnalysisDetail from './SaleAnalysisDetail';
 
@@ -10,10 +10,10 @@ export default function SaleAnalysisView() {
     return (
         <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', justifyItems: 'center', alignItems: 'center'}}>
             <Box sx={{ width: '500px'}}>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DemoContainer components={['StaticDateTimePicker']}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DemoContainer components={['StaticDatePicker']}>
                         <DemoItem>
-                            <StaticDateTimePicker defaultValue={dayjs('2022-04-17T15:30')} />
+                            <StaticDatePicker defaultValue={dayjs('2022-04-17')} />
                         </DemoItem>
                     </DemoContainer>
                 </LocalizationProvider>
