1d3e8584cd04ff8afe3172736f309f568f52e7e4
Gayeong98 <114125163+Gayeong98@users.noreply.github.com>
2023-05-19 16:57:17 +0900

    Kimgayeong (#30)
    fix: Store(post, patch, storeCard)

--- src/views/Store/PostStoreView/index.tsx
+++ src/views/Store/PostStoreView/index.tsx
@@ -22,7 +22,8 @@ export default function PostStoreView() {
 
     const accessToken = cookies.accessToken;
 
-    const imageRef = useRef<HTMLInputElement | null>(null);
+    const StoreImageRef = useRef<HTMLInputElement | null>(null);
+    const LogoImageRef = useRef<HTMLInputElement | null>(null);
 
     const [postStore, setPostStore] = useState<PostStoreResponseDto | null>(null);
     const [storeCloseTime, setStoreCloseTime] = useState<string>('');
@@ -73,6 +74,17 @@ export default function PostStoreView() {
             .catch((error) => storeLogoUploadErrorHandler(error));
     }
 
+
+    const onStoreImageUploadButtonHandler = () => {
+        if (!StoreImageRef.current) return;
+        StoreImageRef.current.click();
+    }
+
+    const onLogoImageUploadButtonHandler = () => {
+        if(!LogoImageRef.current) return;
+        LogoImageRef.current.click();
+    }
+
     const onWriteHandler = () => {
         if (!storeName.trim() || !storeOpenTime.trim() || !storeCloseTime.trim()) {
             alert('모든 내용을 입력해주세요.');
@@ -82,15 +94,8 @@ export default function PostStoreView() {
         setNavigation(Navigation.Store);
     }
 
-    const onStoreNameChangeHandler = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-        const value = event.target.value;
-        setStoreName(value);
-    }
 
-    const onImageUploadButtonHandler = () => {
-        if (!imageRef.current) return;
-        imageRef.current.click();
-    }
+
 
     //          Response Handler          //
 
@@ -139,32 +144,37 @@ export default function PostStoreView() {
 
     return (
         <Box sx={{ display: 'flex', height: '100%' }}>
-            <Box>
-                <Typography>점포명</Typography>
-                <Input placeholder='점포명을 입력하세요.' onChange={(event) => setStoreName(event.target.value)} />
-                <Typography>오픈 시간</Typography>
-                <Input placeholder='오픈 시간을 입력하세요.' onChange={(event) => setStoreOpenTime(event.target.value)} />
-                <Typography>마감 시간</Typography>
-                <Input placeholder='마감 시간을 입력하세요.' onChange={(event) => setStoreCloseTime(event.target.value)} />
-                <Typography>점포 이미지</Typography>
-                <IconButton onClick={() => onImageUploadButtonHandler()}>
-                    <ImageOutlinedIcon />
-                    <input ref={imageRef} hidden type='file' accept='image/*' onChange={(event) => storeImageUploadChangeHandler(event)} />
-                </IconButton>
+<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
+                <Typography sx={{ mt: '2vh', ml: '2vh', display: 'flex', fontSize: '2vh' }}>점포명</Typography>
+                <Input sx={{ ml: '2vh', display: 'flex' }} placeholder={storeName} onChange={(event) => setStoreName(event.target.value)} />
+                <Typography sx={{ ml: '2vh', display: 'flex', fontSize: '2vh' }}>오픈 시간</Typography>
+                <Input sx={{ ml: '2vh', display: 'flex' }} placeholder={storeOpenTime} onChange={(event) => setStoreOpenTime(event.target.value)} />
+                <Typography sx={{ ml: '2vh', display: 'flex', fontSize: '2vh' }}>마감 시간</Typography>
+                <Input sx={{ ml: '2vh', display: 'flex' }} placeholder={storeCloseTime} onChange={(event) => setStoreCloseTime(event.target.value)} />
+                <Typography sx={{ ml: '2vh', display: 'flex', fontSize: '2vh' }}>
+                    점포 이미지
+                    <IconButton onClick={() => onStoreImageUploadButtonHandler()}>
+                        <ImageOutlinedIcon />
+                        <input ref={StoreImageRef} hidden type='file' accept='image/*' onChange={(event) => storeImageUploadChangeHandler(event)} />
+                    </IconButton>
+                </Typography>
                 <Box sx={{ width: '100%' }} component='img' src={storeImgUrl} />
-                <Typography>점포 로고 이미지</Typography>
-                <IconButton onClick={() => onImageUploadButtonHandler()}>
-                    <ImageOutlinedIcon />
-                    <input ref={imageRef} hidden type='file' accept='image/*' onChange={(event) => storeLogoUploadChangeHandler(event)} />
-                </IconButton>
+                <Typography sx={{ ml: '2vh', display: 'flex', fontSize: '2vh' }}>점포 로고 이미지
+                    <IconButton sx={{ flex: 1 }} onClick={() => onLogoImageUploadButtonHandler()}>
+                        <ImageOutlinedIcon />
+                        <input ref={LogoImageRef} hidden type='file' accept='image/*' onChange={(event) => storeLogoUploadChangeHandler(event)} />
+                    </IconButton>
+                </Typography>
                 <Box sx={{ width: '100%' }} component='img' src={storeLogoUrl} />
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <IconButton sx={{ flex: 1 }} onClick={() => setNavigation(Navigation.Store)}>
+                        <Typography>뒤로가기</Typography>
+                    </IconButton>
+                    <IconButton sx={{ flex: 1 }} onClick={onWriteHandler}>
+                        <Typography>등록</Typography>
+                    </IconButton>
+                </Box>
             </Box>
-            <IconButton onClick={()=>setNavigation(Navigation.Store)}>
-                <Typography>뒤로가기</Typography>
-            </IconButton>
-            <Fab sx={{ position: 'fixed', bottom: '200px', right: '248px', backgroundColor: '#999999' }} onClick={onWriteHandler}>
-                <AddBusinessIcon />
-            </Fab>
         </Box>
     )
 }
