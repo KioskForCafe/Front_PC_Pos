b501e4ff6ad2c51c9402ebd6aed69f746b99a9df
Gayeong98 <114125163+Gayeong98@users.noreply.github.com>
2023-05-19 17:45:08 +0900

    Kimgayeong (#31)
    fix: NavigationBar: 로그아웃/매장 리스트로 돌아가는 기능 추가
    fix: patchStoreView: store로 돌아갈 때 store 정보가 reset되도록 설정

--- src/views/NavigationBar/index.tsx
+++ src/views/NavigationBar/index.tsx
@@ -1,8 +1,8 @@
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import CircleIcon from '@mui/icons-material/Circle';
-import { AppBar, Box, Toolbar, Typography, FormControl, OutlinedInput, Button, Icon, IconButton } from '@mui/material'
+import { AppBar, Box, Toolbar, Typography, FormControl, OutlinedInput, Button, Icon, IconButton, Menu, MenuItem } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import { useStoreStore } from '../../stores';
+import { useNavigationStore, useStoreStore } from '../../stores';
 import dayjs, { Dayjs } from 'dayjs';
 import axios, { AxiosResponse } from 'axios';
 import { useCookies } from 'react-cookie';
@@ -10,17 +10,47 @@ import { GET_STORE_URL } from '../../constants/api';
 import { authorizationHeader } from '../../constants/api';
 import ResponseDto from '../../apis/response';
 import { GetStoreResponseDto } from '../../apis/response/store';
+import useStore from '../../stores/user.store';
+import { Navigation } from '../../constants/navigationEnum';
+import useNavigation from '../../stores/navigation.store';
 
 
 export default function NavigationBar() {
 
-    const { store } = useStoreStore();
+    const {setNavigation} = useNavigationStore();
+
+    const { store, resetStore } = useStoreStore();
+    const { user, setUser, resetUser } = useStore();
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [isStoreExist, setIsStoreExist] = useState<boolean>(false);
     const [currentTime, setCurrentTime] = useState(dayjs());
+
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const userMenuOpen = Boolean(anchorEl);
+    const handleUserMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleUserMenuClose = () => {
+        setAnchorEl(null);
+    };
 
     const [cookies] = useCookies();
 
     const accessToken = cookies.accessToken;
+
+    //          Event Handler           //
+
+    const onLogoutButtonHandler = () => {
+        resetUser();
+        resetStore();
+        setNavigation(Navigation.AuthenticationView);
+    }
+
+    const onStoreView = () => {
+        setNavigation(Navigation.Store);
+    }
 
     //          Use Effect           //
 
@@ -29,7 +59,7 @@ export default function NavigationBar() {
             setCurrentTime(dayjs());
         }, 1000);
         return () => {
-            clearInterval(interval); 
+            clearInterval(interval);
         };
     }, []);
 
@@ -37,17 +67,28 @@ export default function NavigationBar() {
         if (store && store?.storeOpenTime !== null && store?.storeCloseTime !== null) {
             setIsOpen(currentTime.hour() >= store.storeOpenTime && currentTime.hour() < store.storeCloseTime);
         }
+        if (!store) setIsOpen(false);
     }, [store, currentTime]);
 
+    useEffect(() => {
+        if (store) {
+            setIsStoreExist(true);
+        }
+        if(!store) setIsStoreExist(false);
+    }, [store])
 
     return (
         <Box sx={{ zIndex: 1, position: 'relative', top: '0', height: '7vh' }}>
             <AppBar variant='outlined' position='relative' sx={{ height: '100%', justifyContent: 'center', p: '0px 100px', backgroundColor: '#383947' }}>
                 <Toolbar>
-                    <Typography
+                    {isStoreExist ? <Typography
                         variant="h6"
                         component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block', color: '#ffffff' } }}>Kiosk Project</Typography>
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block', color: '#ffffff' } }}>{store?.storeName}</Typography> :
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block', color: '#ffffff' } }}>Kiosk Project</Typography>}
                     <Box sx={{ display: 'flex' }}>
                         <Typography sx={{ fontSize: '15px', mt: '10px' }}>{currentTime.format('YYYY-MM-DD hh:mm')}</Typography>
                         {isOpen ? <IconButton sx={{ fontSize: '10px', ml: '10px', color: '#13ba50' }}>
@@ -58,10 +99,19 @@ export default function NavigationBar() {
                         <Typography sx={{ fontSize: '15px', mt: '10px' }}>
                             {isOpen ? '영업중' : '영업 종료'}
                         </Typography>
-                        <IconButton sx={{ fontSize: 'large', ml: '10px', color: '#ffffff' }}>
+                        <IconButton sx={{ fontSize: 'large', ml: '10px', color: '#ffffff' }} onClick={handleUserMenuClick}>
                             <MoreHorizIcon />
                         </IconButton>
                     </Box>
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={userMenuOpen}
+                        onClose={handleUserMenuClose}
+                        onClick={handleUserMenuClose}
+                    >
+                        <MenuItem onClick={() => onLogoutButtonHandler()}>로그아웃</MenuItem>
+                        <MenuItem onClick={() => onStoreView()}>매장 리스트 보기</MenuItem>
+                    </Menu>
                 </Toolbar>
             </AppBar>
         </Box>
